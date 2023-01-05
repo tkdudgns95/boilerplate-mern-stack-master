c9bc58bbc4f711ebb444e634f2a6fca9e5e6d569
tkdudgns95 <tkdudgns95@naver.com>
2023-01-05 14:19:14 +0900

    이미지 파일을 서버로 보내기

---
+++ boilerplate-mern-stack-master/server/routes/product.js
@@ -0,0 +1,19 @@
+const { application } = require('express');
+const express = require('express');
+const router = express.Router();
+
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res) => {
+
+    // 가져온 이미지를 저장을 해주면 된다.
+    
+
+})
+
+
+
+module.exports = router;
