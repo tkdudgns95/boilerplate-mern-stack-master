05b54f02145a2d5b839c4c808069d4bd60cd926d
tkdudgns95 <tkdudgns95@naver.com>
2023-01-06 14:21:15 +0900

    multer를 이용한 이미지 저장

--- boilerplate-mern-stack-master/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const { TextArea } = Input;
+
+const Continents = [
+    {key:1, value: "Africa" },
+    {key:2, value: "Europe" },
+    {key:3, value: "Asia" },
+    {key:4, value: "North America" },
+    {key:5, value: "South America" },
+    {key:6, value: "Australia" },
+    {key:7, value: "Antarctica" }
+]
+
+
+
+
+
 
 function UploadProductPage() {
 
@@ -24,6 +40,9 @@ function UploadProductPage() {
         setPrice(event.currentTarget.value)
     }
 
+    const continentChangeHandler = (event) => {
+        setContinent(event.currentTarget.value)
+    }
 
     return (
         <div style={{maxWidth: '700px', margin: '2rem auto' }}>
@@ -33,6 +52,12 @@ function UploadProductPage() {
             
             <Form>
                 {/* DropZone */}
+
+
+
+            <FileUpload />
+
+
             <br />
             <br />
             <label>이름</label>
@@ -44,11 +69,15 @@ function UploadProductPage() {
             <br />
             <br />
             <label>가격($)</label>
-            <Input type="number" onChange={priceChangeHandler} value={price}/>
+            <Input type="number" onChange={priceChangeHandler} value={Price} />
             <br />
             <br />
-            <select>
-                <option></option>
+            <select onChange={continentChangeHandler} value={Continent}>
+                {Continents.map(item => (
+                    <option key={item.key} value={Continent}> {item.value}</option>
+                ))}
+               
+
             </select>
             <br />
             <br />
