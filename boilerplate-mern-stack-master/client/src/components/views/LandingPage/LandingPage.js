ce887297d5bf25447e5a7c67b0f6e340c059dbf6
tkdudgns95 <tkdudgns95@naver.com>
2023-01-07 12:50:59 +0900

    이미지 슬라이더 만들기

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,75 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
+import { Icon, Col, Card, Row,Carousel } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
 
+    const [Products, setProducts] = useState([])
+
+
+
     useEffect(() => {
 
-        
+        axios.post('/api/product/products', )
+            .then(response => {
+                if (response.data.success) {
+                    
+                    setProducts(response.data.productInfo)
+
+                } else {
+                    alert(" 상품들을 가져오는데 실패 했습니다.")
+                }
+            })
 
     }, [])
 
 
+    const renderCards = Products.map((product, index) => {
+
+        
+
+        return <Col lg={6} md={8} xs={24} key={index}>
+
+        <Card
+            cover={<ImageSlider images={product.images} />}
+        >
+            <Meta 
+                title={product.title}
+                description={`$${product.price}`}
+            />
+        </Card>
+        </Col>
+    })
+
+
 
     return (
-        <div>
-            Laning Page
+        <div style={{ width: '75%', margin: '3rem auto'}}>
+
+            <div style={{ textAlign: 'center'}}>
+                <h2>Let's Travel Anywhere<Icon type="rocket" /></h2>
+            </div>
+
+            {/* Filter */}
+
+            {/* Search */}
+
+            {/* Cards */}
+
+            <Row gutter={[16, 16]}>
+                {renderCards}
+            </Row>
+            
+
+
+
+
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <button>더보기</button>
+            </div>
         </div>
     )
 }
