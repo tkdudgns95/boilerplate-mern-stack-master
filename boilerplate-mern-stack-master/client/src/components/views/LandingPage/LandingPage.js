47fa22b81ad4fd7168b32e1df05a1751d07f7748
tkdudgns95 <tkdudgns95@naver.com>
2023-01-09 04:46:28 +0900

    체크 박스 필터 만들기#1

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { Icon, Col, Card, Row,Carousel } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import ImageSlider from '../../utils/ImageSlider';
+import Checkbox from './Sections/CheckBox';
+import { continents } from './Sections/Datas';
 
 function LandingPage() {
 
@@ -85,7 +87,18 @@ function LandingPage() {
 
             {/* Filter */}
 
+
+            {/* CheckBox */}
+                <Checkbox list={continents} />
+
+            {/* RadioBox */}
+
+
             {/* Search */}
+
+
+
+
 
             {/* Cards */}
 
