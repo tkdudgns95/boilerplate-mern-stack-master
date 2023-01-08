bd2c7a9053064cc6b6c1a367d4c665cf652c9d43
tkdudgns95 <tkdudgns95@naver.com>
2023-01-08 22:14:04 +0900

    더보기 버튼 만들기 #2

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -10,7 +10,9 @@ function LandingPage() {
     const [Products, setProducts] = useState([])
     const [Skip, setSkip] = useState(0)
     const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
 
+    
     useEffect(() => {
 
         let body = {
@@ -18,29 +20,44 @@ function LandingPage() {
             limit: Limit
         }
 
-
-        axios.post('/api/product/products', body)
-            .then(response => {
-                if (response.data.success) {
-                    
-                    setProducts(response.data.productInfo)
-
-                } else {
-                    alert(" 상품들을 가져오는데 실패 했습니다.")
-                }
-            })
+        getProducts(body)
 
     }, [])
 
+    const getProducts = (body) => {
+        axios.post('/api/product/products', body)
+        .then(response => {
+            if (response.data.success) {
+                if(body.loadMore) {
+                    setProducts([...Products, ...response.data.productInfo])
+                } else {
+                    setProducts(response.data.productInfo)
+                }
+                setPostSize(response.data.postSize)
+            } else {
+                alert(" 상품들을 가져오는데 실패 했습니다.")
+            }
+        })
+    }
 
     const loadMoreHandler = () => {
-        
+
+        let skip = Skip + Limit
+            
+        let body = {
+            skip: Skip,
+            limit: Limit,
+            loadMore: true
+        }
+
+        getProducts(body)
+            
+        setSkip(skip) 
+
     }
 
 
     const renderCards = Products.map((product, index) => {
-
-        
 
         return <Col lg={6} md={8} xs={24} key={index}>
 
@@ -76,13 +93,15 @@ function LandingPage() {
                 {renderCards}
             </Row>
             
-
-
-
             <br />
-            <div style={{ display: 'flex', justifyContent: 'center'}}>
-                <button onClick={loadMoreHandler}>더보기</button>
-            </div>
+
+            {PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'center'}}>
+                    <button onClick={loadMoreHandler}>더보기</button>
+                </div>
+            
+            }
+
         </div>
     )
 }
