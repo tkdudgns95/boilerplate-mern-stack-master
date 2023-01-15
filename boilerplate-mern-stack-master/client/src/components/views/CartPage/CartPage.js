a2e13f225b2fea0ed62ecf411f00f874b3c7024b
tkdudgns95 <tkdudgns95@naver.com>
2023-01-15 13:35:15 +0900

    카트에 담긴 상품 정보들을 데이터베이스에서 가져오기#2

---
+++ boilerplate-mern-stack-master/client/src/components/views/CartPage/CartPage.js
@@ -0,0 +1,34 @@
+import { PromiseProvider } from 'mongoose'
+import React,{ useEffect } from 'react'
+import { propfind } from '../../../../../server/routes/users'
+import { useDispatch } from 'react-redux';
+import { getCartItems } from '../../../_actions/user_actions';
+
+function CartPage(props) {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        let cartItems=[]
+
+    // 리덕스 User state안에 cart 안에 상품이 들어있는지 확인
+
+    if(props.user.userData && props.user.userData.cart) {
+        if(props.user.userData.cart.length > 0) {
+            props.user.userData.cart.forEach(item => {
+                cartItems.push(item.id)
+            })
+
+            dispatch(getCartItems(cartItems, props.user.userData.cart)) 
+        }
+    }
+}, [props.user.userData])
+
+return (
+    <div>
+        CartPage
+    </div>
+    )
+}
+
+export default CartPage
