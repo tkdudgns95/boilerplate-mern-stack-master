ba97f242104b406e2418d4dfb5aca9e72ef307a6
tkdudgns95 <tkdudgns95@naver.com>
2023-01-16 20:40:41 +0900

    카트에 들어있는 상품 지우기

--- boilerplate-mern-stack-master/client/src/components/views/CartPage/CartPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/CartPage/CartPage.js
@@ -1,11 +1,13 @@
-import { PromiseProvider } from 'mongoose'
-import React,{ useEffect } from 'react'
-import { propfind } from '../../../../../server/routes/users'
+
+import React,{ useEffect,useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { getCartItems } from '../../../_actions/user_actions';
+import { getCartItems, removeCartItem } from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
 
 function CartPage(props) {
     const dispatch = useDispatch();
+
+    const [Total, setTotal] = useState(0)
 
     useEffect(() => {
 
@@ -20,13 +22,44 @@ function CartPage(props) {
             })
 
             dispatch(getCartItems(cartItems, props.user.userData.cart)) 
+            .then(response => {calculateTotal(response.payload) })
         }
     }
 }, [props.user.userData])
 
+let calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map(item => {
+        total += parseInt(item.price, 10) * item.quantity
+    })
+
+     
+     setTotal(total)  
+}
+
+let removeFromCart = (productId) => {
+
+    dispatch(removeCartItem(productId))
+    .then(response => {
+
+    })
+}
+
+
+
 return (
-    <div>
-        CartPage
+    <div style={{ width: '85%', margin: '3rem auto' }}>
+        <h1>My Cart</h1>
+        
+        <div>
+            <UserCardBlock products={props.user.cartDetail} />
+        </div>
+        <div style={{ marginTop: '3rem' }}>
+            <h2>Total Amount: ${Total}</h2>
+
+
+        </div>
     </div>
     )
 }
