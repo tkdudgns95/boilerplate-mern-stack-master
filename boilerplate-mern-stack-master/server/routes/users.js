40d01e3c49d2f1521384d2aafd5ea031742ff811
tkdudgns95 <tkdudgns95@naver.com>
2023-01-18 10:38:53 +0900

    User Collection안에 History필드에 간단한 결제정보 넣어주기.

--- boilerplate-mern-stack-master/server/routes/users.js
+++ boilerplate-mern-stack-master/server/routes/users.js
@@ -151,16 +151,37 @@ router.get('/removeFromCart', auth, (req,res) => {
                 return res.status(200).json({
                     productInfo,
                     cart
-                    
+
                 })
             })
         }
     )
+})
+
+router.post('/successBuy', auth, (req,res) => {
+
+   //1. User Collection 안에 History 필드 안에 간단한 결제 정보 넣어주기
+    let history = [];
+    let transactionData = {};
+
+    req.body.cartDetail.forEach((item) => {
+        history.push({
+            dateOfPurchase: Date.now(),
+            name: item.title,
+            id: item._id,
+            price: item.price,
+            quantity: item.quantity,
+            paymentId: req.body.paymentData.paymentID
+        })
+    })
+
+   //2. Payment Collection안에 자세한 결제 정보들 넣어주기
 
 
 
-  
+   //3. Product Collection안에 있는 sold 필드 정보 업데이트 시켜주기.
 
 })
+
 
 module.exports = router;
